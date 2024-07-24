751b4fa3fe599dfdd52a5c16764fe48df94f3f9d
Jean-Philippe Evrard <open-source@a.spamming.party>
2024-07-24 15:34:16 +0200

    Make it easily testable
    I was annoyed by creating files and loading each test manually.
    So instead, using pytest will run all my tests easily.

---
+++ README.md
@@ -0,0 +1,13 @@
+# JSON Validation example
+
+## How to test
+
+`pytest` from this repo.
+
+## How to validate a jsonschema with a user provided file
+
+`python validate.py <filename>`
+
+## How to generate datastructures from jsonschema
+
+Depends on your language. Please google.
---
+++ tests/test_examples.py
@@ -0,0 +1,43 @@
+import pytest
+import json
+import jsonschema
+from jsonschema import validate
+import sys
+import os
+
+@pytest.fixture(scope='session')
+def schema():
+    with open('golden_rules_audit_schema.json','r') as sf:
+        return json.load(sf)
+
+def test_schema(schema):
+    # Test validity of default schema
+    try:
+        validate(instance={}, schema=schema)
+    except jsonschema.exceptions.ValidationError:
+        pass
+
+class TestPassing:
+    def test_full(self, schema):
+        with open('tests/example-pass--full.json', 'r') as f:
+            j = json.load(f)
+        validate(instance=j, schema=schema)
+    
+    def test_empty_controls(self, schema):
+        with open('tests/example-pass--empty.json', 'r') as f:
+            j = json.load(f)
+        validate(instance=j, schema=schema)
+
+class TestInvalidFiles:
+    def test_no_controls(self, schema):
+        with open('tests/example-fail--nocontrols.json', 'r') as f:
+            j = json.load(f)
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            validate(instance=j, schema=schema)
+
+    def test_fail_emptycontrol(self, schema):
+        with open('tests/example-fail--emptycontrol.json', 'r') as f:
+            j = json.load(f)
+        with pytest.raises(jsonschema.exceptions.ValidationError):
+            validate(instance=j, schema=schema)
+
